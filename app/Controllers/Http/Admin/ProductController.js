a1e506119c69da359fa81d8552de032d3fa2ee49
mario florence <mflorence1989@gmail>
2019-01-13 04:09:32 -0800

    implement routes and controllers

---
+++ app/Controllers/Http/Admin/ProductController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class ProductController {
+  index(){
+    return 'this is the products page'
+  }
+  store(){
+
+  }
+  create(){
+
+  }
+  show(){
+
+  }
+  edit(){
+
+  }
+  update(){
+
+  }
+  delete(){
+
+  }
+}
+
+module.exports = ProductController
